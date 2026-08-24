65572795ae599258bff108bbeffa3040477919b6
Julián Capdevila <Julicapdevila30@gmail.com>
2026-08-24 19:23:04 -0300

    etapa 2: carga de un producto

--- Program.cs
+++ Program.cs
@@ -1,6 +1,22 @@
-﻿const string NombreComercio = "Carrefour";
-Console.WriteLine($"--- {NombreComercio} ---");
-Console.Write("Nombre del Cajero: ");
-string NombreCajero = Console.ReadLine();
-Console.WriteLine($"Bienvenido {NombreCajero}, Caja Abierta.");
+﻿//const string NombreComercio = "Carrefour";
+//Console.WriteLine($"--- {NombreComercio} ---");
+//Console.Write("Nombre del Cajero: ");
+//string NombreCajero = Console.ReadLine();
+//Console.WriteLine($"Bienvenido {NombreCajero}, Caja Abierta.");
+
+Console.WriteLine("Ingrese un Producto: ");
+string NombreProducto = Console.ReadLine();
+Console.Write("Ingrese el precio del producto: ");
+string PrecioProducto = Console.ReadLine();
+decimal precioDecimal = Decimal.Parse(PrecioProducto);
+
+Console.WriteLine($"Producto: {NombreProducto}, precio: {precioDecimal}");
+
+
+
+
+
+
+
  Console.ReadLine();
+
