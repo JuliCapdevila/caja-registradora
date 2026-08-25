0124028be2d30be0b6416e0199d7d108b1385b96
Julián Capdevila <Julicapdevila30@gmail.com>
2026-08-25 00:04:33 -0300

    etapa 5: medio de pago

--- Program.cs
+++ Program.cs
@@ -42,7 +42,7 @@ do
             break;
 
         default:
-            Console.WriteLine("Opción Invalida, intente nuevamente.");
+            Console.WriteLine("Opcion Invalida, intente nuevamente.");
             break;
     }
 
@@ -61,9 +61,51 @@ else if (total > 20000)
 
 decimal totalAPagar = total - plataDescontada;
 
+string opcionPago;
+decimal ajustePago = 0;
+
+do
+{
+    Console.WriteLine("Medio de pago:");
+    Console.WriteLine("1 - Efectivo");
+    Console.WriteLine("2 - Debito");
+    Console.WriteLine("3 - Credito");
+
+    opcionPago = Console.ReadLine();
+
+    switch (opcionPago)
+    {
+        case "1":
+            ajustePago = -(totalAPagar * 10m / 100m);
+            break;
+        case "2":
+            ajustePago = 0;
+            break;
+        case "3":
+            ajustePago = (totalAPagar * 15m / 100m);
+            break;
+        default:
+            Console.WriteLine("Opcion invalida, intente nuevamente.");
+            break;
+    }
+
+} while (opcionPago != "1" && opcionPago != "2" && opcionPago != "3");
+
+totalAPagar = totalAPagar + ajustePago;
+
 Console.WriteLine("=== TICKET FINAL ===");
 Console.WriteLine($"Subtotal: ${total}");
-Console.WriteLine($"Descuento: ${plataDescontada}");
+Console.WriteLine($"Descuento por monto: ${plataDescontada}");
+
+if (opcionPago == "1")
+{
+    Console.WriteLine($"Descuento adicional por efectivo: ${ajustePago}");
+}
+else if (opcionPago == "3")
+{
+    Console.WriteLine($"Recargo por credito: ${ajustePago}");
+}
+
 Console.WriteLine($"Total a cobrar: ${totalAPagar}");
 
 Console.ReadLine();
